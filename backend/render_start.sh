e1759952e8a29529d0718f1520bcf480f9da5a61
Darith <dychandarith@gmail.com>
2026-05-21 09:03:52 +0700

    Update render_start.sh

--- backend/render_start.sh
+++ backend/render_start.sh
@@ -8,4 +8,4 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting server..."
-gunicorn saveur.wsgi:application
+gunicorn saveur.wsgi:application --bind 0.0.0.0:$PORT
